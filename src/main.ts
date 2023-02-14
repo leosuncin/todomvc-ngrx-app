c9377f88790c7f2b105fc8b00348a9d8e0ec72bd
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2023-02-14 16:21:02 -0600

    feat: install @ngrx/store

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideEffects } from '@ngrx/effects';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideStore(), provideEffects(), provideStoreDevtools()],
+}).catch((err) => console.error(err));
